c25ff3ed2b7e59a6d9f5bb1f2b49262b3d9118b8
Håkon <haakonfs@hotmail.com>
2022-11-22 16:15:49 +0100

    Fixed folder structure

---
+++ Assets/Scripts/Character/Projectile.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public class Projectile : MonoBehaviour {
+
+	[Range(5, 100)]
+	public float minDestroyTime;
+	public float maxDestroyTime;
+
+	public bool destroyOnImpact = false;
+
+
+	[Header("Impact Effect Prefabs")]
+	public Transform [] metalImpactPrefabs;
+
+	private float damage = 30f;
+
+	private void Start ()
+	{
+		
+	}
+
+	//If the bullet collides with anything
+	private void OnCollisionEnter(Collision collision)
+	{
+		//Ignore collisions with other projectiles.
+		if (collision.gameObject.GetComponent<Projectile>() != null)
+			return;
+
+
+		//If destroy on impact is false, start 
+		//coroutine with random destroy timer
+		if (!destroyOnImpact)
+		{
+			StartCoroutine(DestroyTimer());
+		}
+		//Otherwise, destroy bullet on impact
+		else
+		{
+			Destroy(gameObject);
+		}
+
+
+		//If bullet collides with "Metal" tag
+		if (collision.transform.tag == "Metal") 
+		{
+			collision.collider.GetComponent<EnemyManager>().Hit(damage);
+
+			//Instantiate random impact prefab from array
+			Instantiate (metalImpactPrefabs [Random.Range 
+				(0, metalImpactPrefabs.Length)], transform.position, 
+				Quaternion.LookRotation (collision.contacts [0].normal));
+			//Destroy bullet object
+			Destroy(gameObject);
+		}
+	}
+
+	private IEnumerator DestroyTimer()
+	{
+		//Wait random time based on min and max values
+		yield return new WaitForSeconds
+			(Random.Range(minDestroyTime, maxDestroyTime));
+		//Destroy bullet object
+		Destroy(gameObject);
+	}
+
+}
