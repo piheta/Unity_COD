3d7638ad5ab26cc041fb04e54e7dc41b263f5693
Richileu Alphonso Bailey <richileb@stud.ntnu.no>
2022-11-24 02:16:44 +0100

    removed unused files in ui folder

--- Assets/ui/PickupWithScriptableObject.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class PickupWithScriptableObject : ScriptableObject
-{
-    public abstract void Apply(GameObject target);
-}
